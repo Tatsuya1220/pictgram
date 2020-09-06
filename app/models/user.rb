27b79fd89b54bdd52284f20f3cda07193e114b66
”hirayama <hrymtty0311@yahoo.co.jp>
2020-09-06 09:55:49 +0000

    rails04課題内容

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   
+  VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
+  validates :password, length: { minimum: 8, maximum: 32 }
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+  VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z.\d]+.[a-z]+\z/i
+  validates :email, format: { wish: VALID_EMAIL_REGEX }
+  validates :name, length: {maximum: 15 }
   has_secure_password
 end
